c819e3cd9b05081f11878ede5ff9dbf511ff8bdc
[Ajay] <cyrisstudio@gmail.com>
2021-09-17 10:54:51 +0530

    ""

--- app/src/main/java/com/cyris/createphoto/SplashScreen.java
+++ app/src/main/java/com/cyris/createphoto/SplashScreen.java
@@ -14,6 +14,7 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         getSupportActionBar().hide();
         Handler handler = new Handler();
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
